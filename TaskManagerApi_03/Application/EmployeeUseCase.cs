c1eda3ae975d5975b3d7879e2cebb3ac8e326f6d
jose Angel <josegallardo.cor@gmail.com>
2026-07-12 23:08:46 -0600

    feat: casos de uso para employee creacion y consulta

---
+++ TaskManagerApi_03/Application/EmployeeUseCase.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskManagerApi_03.Domain;
+using TaskManagerApi_03.Dtos.Employees;
+using TaskManagerApi_03.Dtos.Tasks;
+using TaskManagerApi_03.Infrastructure;
+
+namespace TaskManagerApi_03.Application
+{
+    public class EmployeeUseCase
+    {
+        private readonly AppDbContext _context;
+        public EmployeeUseCase(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<EmployeeDto> Create(CreateEmployee request)
+        {
+            var existingEmployee = await _context.Employees.FirstOrDefaultAsync(e => e.Email == request.Email);
+            if (existingEmployee != null)
+            {
+                throw new InvalidOperationException("El empleado con el mismo correo electrónico ya existe.");
+            }
+
+            var employee = new Employee(
+                request.Name,
+                request.Email,
+                request.Department
+                );
+            await _context.AddAsync(employee);
+            await _context.SaveChangesAsync();
+            return MapToDto(employee);
+        }
+        public async Task<EmployeeDto?> GetById(Guid id)
+        {
+            var employee = await _context.Employees.FindAsync(id);
+            if (employee == null)
+            {
+                return null;
+            }
+            return MapToDto(employee);
+        }
+        public async Task<IEnumerable<EmployeeDto>> GetAll()
+        {
+            var employees = await _context.Employees.Include(e => e.Tasks).ToListAsync();
+            return employees.Select(MapToDto);
+        }
+        private static EmployeeDto MapToDto(Employee employee) => new EmployeeDto
+        {
+            Id = employee.Id,
+            Name = employee.Name,
+            Email = employee.Email,
+            Department = employee.Department,
+            IsActive = employee.IsActive,
+            Tasks = employee.Tasks.Select(t => new TaskDto
+            {
+                Id = t.Id,
+                Title = t.Title,
+                Description = t.Description,
+                DueDate = t.DueDate,
+            }).ToList()
+        };
+    }
+}
